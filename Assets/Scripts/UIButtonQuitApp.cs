a0483292212e1b6f1b8d67c44ad48d239a8aeab0
Vivien Pfrimmer <vpfrimmer@methodinthemadness.eu>
2018-01-26 20:44:47 +0100

    chore(*) : init commit and project hierarchy

---
+++ Assets/Scripts/UIButtonQuitApp.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+
+
+///=================================================================================================================
+///                                                                                                       <summary>
+///  UIButtonQuitApp is attached to a button to quit the application									 </summary>
+///
+///=================================================================================================================
+public class UIButtonQuitApp : MonoBehaviour 
+{
+	///-------------------------------------------------------                                                 <summary>
+	/// Called by UI                                                                            </summary>
+	///-------------------------------------------------------
+	public void OnClicked () 
+	{
+        Application.Quit();
+	}
+
+    public void OnBackToMenu()
+    {
+        SceneManager.LoadScene("S_Menu");
+    }
+}
+
---
+++ Assets/Scripts/UIButtonStartApp.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+///=================================================================================================================
+///                                                                                                       <summary>
+///  UIButtonStartApp is attached to a menu button to load the next level in the level list.  
+///  Usually the menu ist the first level, the first game level is the second one.                         </summary>
+///
+///=================================================================================================================
+public class UIButtonStartApp : MonoBehaviour
+{
+    ///-------------------------------------------------------                                                 <summary>
+    /// Called by UI                                                                            </summary>
+    ///-------------------------------------------------------
+    public void OnClicked()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+	
+	public void LaunchNamedLevel(string levelName)
+	{
+        SceneManager.LoadScene(levelName);
+	}
+}
+
+
