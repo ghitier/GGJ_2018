d010ea0dd58488075bc5c254f6dc923f421a88ac
Unknown <guillaume.hitier@epitech.eu>
2018-01-27 15:36:21 +0100

    first character tests

---
+++ Assets/Scripts/BodyPartRandomizer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BodyPart
+{
+    public string name;
+    public Sprite sprite;
+    public Gender gender = Gender.Neutral;
+}
+
+public class BodyPartRandomizer : MonoBehaviour {
+    [SerializeField]
+    public List<BodyPart> sprites = new List<BodyPart>();
+    private SpriteRenderer renderer;
+
+    void Awake()
+    {
+        
+        renderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void SelectRandom()
+    {
+        Debug.Log("Randomizing " + renderer.name + " sprite...");
+        Sprite sprite = sprites[Random.Range(1, sprites.Count) - 1].sprite;
+        renderer.sprite = sprite;
+    }
+}
---
+++ Assets/Scripts/CharacterRandomizer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Gender
+{
+    Male,
+    Female,
+    Neutral
+}
+
+public class CharacterRandomizer : MonoBehaviour {
+
+    public Gender gender;
+    public bool randomize;
+    private List<BodyPartRandomizer> _parts = new List<BodyPartRandomizer>();
+
+    private void Awake()
+    {
+        _parts = new List<BodyPartRandomizer>(GetComponentsInChildren<BodyPartRandomizer>());
+    }
+
+    private void Start()
+    {
+        if (randomize == true)
+        {
+            _parts.ForEach(part => part.SelectRandom());
+        }
+    }
+}
