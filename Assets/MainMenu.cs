db3da493349e375f01a5418d2fdb15b974d95d87
Unknown <guillaume.hitier@epitech.eu>
2018-01-28 14:34:06 +0100

    Added Main Menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+    
+	// Use this for initialization
+    void Start()
+    {
+
+    }
+
+	public void Play()
+    {
+        Application.LoadLevel("S_Game");
+    }
+
+    public void Quit()
+    {
+        if (Application.isEditor)
+            EditorApplication.isPlaying = false;
+        else
+            Application.Quit();
+    }
+}
